5be597dd2c19693c311c22da218c9dd7c4455460
Willian Vieira Cardoso <wvcardoso@gmail.com>
2018-04-22 01:13:58 -0300

    .

--- plugins/willian-plugin/contents/vcenter.sh
+++ plugins/willian-plugin/contents/vcenter.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 
 # variables set by plugin properties:
-: ${RD_CONFIG_HOSTT:?"account plugin property not specified"}
+: ${RD_CONFIG_HOST:?"account plugin property not specified"}
 : ${RD_CONFIG_USER:?"region plugin property not specified"}
 : ${RD_CONFIG_PASSWORD:?"url plugin property not specified"}
 
-#
-# Generate node data here.
-#
+
+echo '
+{
+  "madmartigan.local": {
+    "tags": "local,server",
+    "osFamily": "unix",
+    "username": "greg",
+    "osVersion": "10.10.3",
+    "osArch": "x86_64",
+    "description": "${RD_CONFIG_HOST} - ${RD_CONFIG_USER} - ${RD_CONFIG_PASSWORD} - Rundeck server node",
+    "hostname": "madmartigan.local",
+    "nodename": "madmartigan.local",
+    "osName": "Mac OS X"
+  },
+  "test": {
+    "tags": "alphabet, soup",
+    "osFamily": "unix",
+    "ssh-key-storage-path": "keys/testkey1.pem",
+    "username": "vagrant",
+    "osVersion": "10.10.3",
+    "osArch": "x86_64",
+    "description": "Rundeck server node",
+    "hostname": "192.168.33.12",
+    "nodename": "test",
+    "osName": "Mac OS X"
+  }
+}
+'
 
 exit $?
